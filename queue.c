00d3f83fa9a8598e30f1b4610d58d87505e84fa7
DotandLog <roy77369@gmail.com>
2024-03-01 07:16:37 +0000

    Complete queue functions and improve queue tests with some limitations
    This commit introduces completed implementations for `q_new()`, `q_free()`,
    `q_insert_head()`, and `q_insert_tail()`. Alongside the new implementations,
    it includes tests that have been successfully passed, ensuring functional
    validity of the implemented features.
    
    However, `q_remove_tail()` and `q_remove_head()` are noted to fail their
    respective tests due to performance constraints; particularly, they do not
    satisfy the constant time complexity requirement. This performance issue
    arises from the underlying data structure's characteristics and will require
    further review and potential redesign to optimize for the expected time
    complexity.

--- queue.c
+++ queue.c
@@ -1,8 +1,8 @@
+#include "queue.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#include "queue.h"
 
 /* Notice: sometimes, Cppcheck would find the potential NULL pointer bugs,
  * but some of them cannot occur. You can suppress them by adding the
@@ -10,44 +10,99 @@
  *   cppcheck-suppress nullPointer
  */
 
-
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *q_head = malloc(sizeof(struct list_head));
+    if (!q_head) {
+        free(q_head);
+        return NULL;
+    }
+    INIT_LIST_HEAD(q_head);
+    return q_head;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *l) {}
+void q_free(struct list_head *l)
+{
+    if (!l) {
+        return;
+    }
+    element_t *entry, *safe;
+    list_for_each_entry_safe (entry, safe, l, list) {
+        q_release_element(entry);
+    }
+    free(l);
+    return;
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head) {
+        return false;
+    }
+    element_t *new_element = malloc(sizeof(element_t));
+    if (!new_element) {
+        return false;
+    }
+    new_element->value = strdup(s);
+    if (!new_element->value) {
+        free(new_element);
+        return false;
+    }
+    list_add(&new_element->list, head);
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
-    return true;
+    return q_insert_head(head->prev, s);
 }
 
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+    element_t *rm_head = list_first_entry(head, element_t, list);
+    if (sp) {
+        strncpy(sp, rm_head->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    list_del(head->next);
+    return rm_head;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head)) {
+        return NULL;
+    }
+    element_t *rm_tail = list_last_entry(head, element_t, list);
+    if (sp) {
+        strncpy(sp, rm_tail->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    list_del(head->prev);
+    return rm_tail;
 }
 
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head || list_empty(head)) {
+        return 0;
+    }
+    int cnt = 0;
+    struct list_head *node;
+    list_for_each (node, head) {
+        cnt++;
+    }
+    return cnt;
 }
 
 /* Delete the middle node in queue */
